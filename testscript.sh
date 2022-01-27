7dd3b213c9120d521d2b5f20f84a93af829cad7f
KyleTaft <97474635+KyleTaft@users.noreply.github.com>
2022-01-27 00:42:23 -0500

    Add files via upload

---
+++ testscript.sh
@@ -0,0 +1,10 @@
+#!/user/taft
+module load root
+root -l Histograms_total_Fe.root
+Oslo2D_Fe69_bkgsub_total ->ProjectionX("projectX_432", 20.0 , 24.0 )
+Oslo2D_Fe69_bkgsub_total ->ProjectionX("projectX_780", 38.0 , 42.0 )
+Oslo2D_Fe69_bkgsub_total ->ProjectionX("projectX_840", 41.0 , 45.0 )
+Oslo2D_Fe69_bkgsub_total ->ProjectionX("projectX_1020", 50.0 , 54.0 )
+Oslo2D_Fe69_bkgsub_total ->ProjectionX("projectX_2865", 142.0 , 146.0 )
+Oslo2D_Fe69_bkgsub_total ->ProjectionX("projectX_3005", 149.0 , 153.0 )
+Oslo2D_Fe69_bkgsub_total ->ProjectionX("projectX_3520", 175.0 , 179.0 )
