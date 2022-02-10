aff6da70e2fa241628b03f8721caec1e50717398
KyleTaft <97474635+KyleTaft@users.noreply.github.com>
2022-02-09 23:34:32 -0500

    Add files via upload

---
+++ 71Co_root_projection.C
@@ -0,0 +1,7 @@
+{
+Oslo2D_Co71_bkgsub_total->ProjectionX("projectX_270",12.0,16.0);
+Oslo2D_Co71_bkgsub_total->ProjectionX("projectX_410",19.0,23.0);
+Oslo2D_Co71_bkgsub_total->ProjectionX("projectX_579",27.0,31.0);
+Oslo2D_Co71_bkgsub_total->ProjectionX("projectX_790",38.0,42.0);
+Oslo2D_Co71_bkgsub_total->ProjectionX("projectX_950",46.0,50.0);
+}
