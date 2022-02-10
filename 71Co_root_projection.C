b20eb9215f2888f95dcf4c49989683de8a88ded1
KyleTaft <97474635+KyleTaft@users.noreply.github.com>
2022-02-09 23:46:29 -0500

    Add files via upload

--- 71Co_root_projection.C
+++ 71Co_root_projection.C
@@ -1,7 +1,7 @@
 {
-Oslo2D_Co71_bkgsub_total->ProjectionX("projectX_270",12.0,16.0);
-Oslo2D_Co71_bkgsub_total->ProjectionX("projectX_410",19.0,23.0);
-Oslo2D_Co71_bkgsub_total->ProjectionX("projectX_579",27.0,31.0);
-Oslo2D_Co71_bkgsub_total->ProjectionX("projectX_790",38.0,42.0);
-Oslo2D_Co71_bkgsub_total->ProjectionX("projectX_950",46.0,50.0);
+Oslo2D_Co71_bkgsub_total->ProjectionY("projectY_270",12.0,16.0);
+Oslo2D_Co71_bkgsub_total->ProjectionY("projectY_410",19.0,23.0);
+Oslo2D_Co71_bkgsub_total->ProjectionY("projectY_579",27.0,31.0);
+Oslo2D_Co71_bkgsub_total->ProjectionY("projectY_790",38.0,42.0);
+Oslo2D_Co71_bkgsub_total->ProjectionY("projectY_950",46.0,50.0);
 }
